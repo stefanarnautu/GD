aba92ce0858a8200c94c16172bfff3fd7da432ac
stefanarnautu <79250177+stefanarnautu@users.noreply.github.com>
2023-04-01 21:38:11 +0300

    ok

--- DriveTheBall/Assets/Assets/Scripts/MouseFollow.cs
+++ DriveTheBall/Assets/Assets/Scripts/MouseFollow.cs
@@ -1,18 +1,34 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class MouseFollow : MonoBehaviour
 {
+    Rigidbody2D rb;
+    Renderer renderer;
+    TilemapCollider2D tc;
+    BoxCollider2D bc;
+    Material material;
+
     Vector3 mousePosition;
     public float moveSpeed = 0.1f;
-    Rigidbody2D rb;
     Vector2 position = new Vector2(0f, 0f);
-    BoxCollider2D bc;
+    float opacity = 0.5f;
     bool start = false;
+    
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         bc = GetComponent<BoxCollider2D>();
+        tc = GetComponent<TilemapCollider2D>();
+        tc.enabled = false;
+
+        renderer = GetComponent<Renderer>();
+        material = new Material(renderer.material);
+        Color color = material.color;
+        color.a = opacity;
+        material.color = color;
+        renderer.material = material;
     }
 
     private void Update()
@@ -26,6 +42,11 @@ public class MouseFollow : MonoBehaviour
             if (Input.GetMouseButtonDown(0))
             {
                 Debug.Log("Mouse Clicked");
+                Color color = material.color;
+                color.a = 1f;
+                material.color = color;
+                renderer.material = material;
+                tc.enabled = true;
                 start = true;
             }
         }
@@ -35,4 +56,5 @@ public class MouseFollow : MonoBehaviour
     {
         rb.MovePosition(position);
     }
+
 }
